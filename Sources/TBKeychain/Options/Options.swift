f27d32456f6981a99e0894ff30d5f401f91dd007
Todd Bowden <toddhokie@gmail.com>
2021-11-17 17:13:38 -0500

    add Items to Options

--- Sources/TBKeychain/Options/Options.swift
+++ Sources/TBKeychain/Options/Options.swift
@@ -12,14 +12,14 @@ public struct Options {
     public var constraints: AccessConstraints
     public var synchronizable: Bool
     public var invisible: Bool
-    public var encrypt: Bool
+    public var encryptItems: Bool
     
-    public init(protection: AccessProtection, constraints: AccessConstraints, synchronizable: Bool, invisible: Bool, encrypt: Bool) {
+    public init(protection: AccessProtection, constraints: AccessConstraints, synchronizable: Bool, invisible: Bool, encryptItems: Bool) {
         self.protection = protection
         self.constraints = constraints
         self.synchronizable = synchronizable
         self.invisible = invisible
-        self.encrypt = encrypt
+        self.encryptItems = encryptItems
     }
     
     public func accessControl(isPrivateKey: Bool = false) throws -> SecAccessControl {
@@ -42,6 +42,22 @@ public struct Options {
 
 public extension Options {
     static var `default`: Options {
-        return Options(protection: .afterFirstUnlockThisDeviceOnly, constraints: AccessConstraints.none, synchronizable: false, invisible: true, encrypt: true)
+        return Options(
+            protection: .afterFirstUnlockThisDeviceOnly,
+            constraints: AccessConstraints.none,
+            synchronizable: false,
+            invisible: true,
+            encryptItems: false
+        )
+    }
+    
+    static var `defaultEncryptItems`: Options {
+        return Options(
+            protection: .afterFirstUnlockThisDeviceOnly,
+            constraints: AccessConstraints.none,
+            synchronizable: false,
+            invisible: true,
+            encryptItems: true
+        )
     }
 }
